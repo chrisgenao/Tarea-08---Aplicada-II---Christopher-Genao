1cfe684e5ce3fc8c6d39424e2052f1a1f7cc6962
chrisgenao <christopher_genao@hotmail.com>
2016-02-07 20:10:45 -0400

    Tarea 8 Realizada

---
+++ SimpleAuth/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SimpleAuth.Default" %>
+
+<html>
+<head>
+  <title>Forms Authentication - Default Page</title>
+</head>
+
+
+<body>
+  <h3>
+    Todo fue un exito, Enel.</h3>
+  <asp:Label ID="Welcome" runat="server" />
+  <form id="Form1" runat="server">
+    <asp:Button ID="Submit1" OnClick="Signout_Click" 
+       Text="Desconectar" runat="server" /><p>
+  </form>
+</body>
+</html>
---
+++ SimpleAuth/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+
+namespace SimpleAuth
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        public void Page_Load(object sender, EventArgs e)
+        {
+            Welcome.Text = "Hello, " + Context.User.Identity.Name;
+        }
+
+        public void Signout_Click(object sender, EventArgs e)
+        {
+            FormsAuthentication.SignOut();
+            Response.Redirect("Logon.aspx");
+        }
+    }
+}
